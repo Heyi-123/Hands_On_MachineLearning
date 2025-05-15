1e36c44544f66d8062e3c1fd781382f701158d1f
Heyi-123 <xuheyi2018@gmail.com>
2025-05-16 07:19:28 +0800

    coding

---
+++ Exercises_Num9.py
@@ -0,0 +1,10 @@
+####################################################################################################################
+#Exercises_Num9: Trainan an SVM classifier on the MINIST dataset. Since SVM classifiers are binary classifiers.
+#you will need to use one-versus-the-rest to classify all 10 digits. You may want to tune the hyperparameters using
+#small validation sets to speed up the process.
+#what accuracy can you reach?
+####################################################################################################################
+
+#############################################################################################
+#analysis:
+#step 1: 
